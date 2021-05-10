dfd7a2986d34d1792adad5c801b16d373cd774dd
Siva Dayalan <sdayalan@gmail.com>
2021-05-11 00:42:42 +0800

    Initial Commit

---
+++ MockBankApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MockBankApp
+//
+//  Created by Siva Dayalan on 11/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
