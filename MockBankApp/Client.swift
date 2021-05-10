751e80014367c790ab51788422966198f1ffa591
Siva Dayalan <sdayalan@gmail.com>
2021-05-11 02:23:09 +0800

    Updated the `Client` with `payTo`, `paidBy` and `topup` functions and test cases.

--- MockBankApp/Client.swift
+++ MockBankApp/Client.swift
@@ -22,14 +22,116 @@ struct Client {
     var owed = [Client: Double]()
 }
 
-extension Client: Hashable {}
-
-extension Client: Equatable {
+extension Client: Hashable {
     static func ==(lhs: Client, rhs: Client) -> Bool {
-        return lhs.name == rhs.name
+        return lhs.name == rhs.name && lhs.balance == rhs.balance
     }
 }
 
+
 typealias Transaction = (amount: Double, debt: Double, owe: Double)
 
+extension Client {
+    
+    /// Pay to a client
+    /// - Parameters:
+    ///   - client: client to be paid
+    ///   - amount: amount to be paid
+    /// - Returns: Resultant Transaction
+    mutating func pay(to client: Client, amount: Double) -> Transaction {
+        var owedValue = 0.0
+        
+        for owedClient in owed {
+            if owedClient.key.name == client.name {
+                owedValue = owedClient.value
+            }
+        }
+        
+        if owedValue > 0 {
+            if amount < owedValue {
+                let value = owedValue - amount
+                owed.updateValue(value, forKey: client)
+                return (0, 0, value)
+            } else {
+                let value = amount - owedValue
+                owed.updateValue(0, forKey: client)
+                return (value, 0, 0)
+            }
+        } else if owedValue == 0 {
+            if balance >=  amount {
+                // Normal Payment
+                balance -= amount
+                return (amount, 0, 0)
+            } else {
+                // Debt Payment
+                if balance > 0 {
+                    let debt = amount - balance // make it postive
+                    let paid = balance
+                    balance = 0
+                    debted.updateValue(debt, forKey: client)
+                    return (paid, debt, 0)
+                } else {
+                    // topup
+                    return (0, 0, 0)
+                }
+            }
+        } else {
+            return (0, 0, 0)
+        }
+    }
+    
+    /// Paid by
+    /// - Parameters:
+    ///   - client: Client who is paying
+    ///   - transaction: Amount paid by client
+    mutating func paid(by client: Client, transaction: Transaction) {
+        let amount = transaction.amount
+        let owe = transaction.owe
+        let debt = transaction.debt
+        
+        if owe >= 0 {
+            // owed by client. Debt to self
+            debted.updateValue(owe, forKey: client)
+        }
+        
+        if debt >= 0 {
+            // debted by client. Owned by self
+            owed.updateValue(debt, forKey: client)
+        }
+        
+        if amount >= 0 {
+            balance += amount
+        }
+    }
+    
+    mutating func topup(_ amount: Double) {
+        balance += amount
+    }
+    
+    /// Pay Debt
+    /// - Parameter client: Client who is expecting to be paid of debts
+    /// - Returns: Resultant Transaction
+    mutating func payDebt(for client: Client) -> Transaction {
+        var debtValue = 0.0
+        
+        for debtedClient in debted {
+            if debtedClient.key.name == client.name {
+                debtValue = debtedClient.value
+            }
+        }
+        
+        if balance > debtValue {
+            balance -= debtValue
+            let transaction = pay(to: client, amount: debtValue)
+            debted.updateValue(0, forKey: client)
+            return transaction
+        } else {
+            let pending = debtValue - balance
+            let transaction = pay(to: client, amount: balance)
+            debted.updateValue(pending, forKey: client)
+            balance = 0
+            return transaction
+        }
+    }
+}
 
--- MockBankAppTests/ClientTest.swift
+++ MockBankAppTests/ClientTest.swift
@@ -17,5 +17,39 @@ class ClientTest: XCTestCase {
         XCTAssertEqual(clientAlice.balance, 100.0)
         XCTAssertEqual(clientBob.balance, 80)
     }
+    
+    func testTransactions() {
+        let firstTransaction = clientBob.pay(to: clientAlice, amount: 50)
+        clientAlice.paid(by: clientBob, transaction: firstTransaction)
+        
+        XCTAssertEqual(clientAlice.balance, 150.0)
+        XCTAssertEqual(clientBob.balance, 30)
+        
+        let secondTransaction = clientBob.pay(to: clientAlice, amount: 100)
+        clientAlice.paid(by: clientBob, transaction: secondTransaction)
 
+        XCTAssertEqual(clientAlice.balance, 180.0)
+        XCTAssertEqual(clientBob.balance, 0)
+        XCTAssertEqual(secondTransaction.debt, 70)
+
+        clientBob.topup(30)
+        let thirdTransaction = clientBob.payDebt(for: clientAlice)
+        clientAlice.paid(by: clientBob, transaction: thirdTransaction)
+
+        XCTAssertEqual(clientAlice.balance, 210.0)
+        XCTAssertEqual(clientBob.balance, 0)
+        
+        let fourthTransaction = clientAlice.pay(to: clientBob, amount: 30)
+        clientBob.paid(by: clientAlice, transaction: fourthTransaction)
+
+        XCTAssertEqual(clientAlice.balance, 210.0)
+        XCTAssertEqual(clientBob.balance, 0)
+
+        clientBob.topup(100)
+        let fifthTransaction = clientBob.payDebt(for: clientAlice)
+        clientAlice.paid(by: clientBob, transaction: fifthTransaction)
+
+        XCTAssertEqual(clientAlice.balance, 220.0)
+        XCTAssertEqual(clientBob.balance, 90.0)
+    }
 }
